fd6d7fbfb5b4ab64f00dcd9d1c12440a2468260f
hiimo <115462482+hiimo@users.noreply.github.com>
2024-08-30 04:15:36 +0300

    Add files via upload

---
+++ harj6-8/config.php
@@ -0,0 +1,13 @@
+<?php
+//***protseduuriline***//
+//sinu andmed
+$db_server = 'localhost';
+$db_andmebaas = 'muusikapood';
+$db_kasutaja = 'hiimo';
+$db_salasona = 'hiimo';
+//ühendus andmebaasiga
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+//ühenduse kontroll
+if(!$yhendus){
+	die('Ei saa ühendust andmebaasiga');
+}
