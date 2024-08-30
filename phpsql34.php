dba2b66781eaa588e204e5126737e7f5477db277
hiimo <115462482+hiimo@users.noreply.github.com>
2024-08-30 03:41:14 +0300

    Update phpsql34.php

--- phpsql34.php
+++ phpsql34.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>spsps</title>
+    <title>harj 3 ja 4</title>
 </head>
 
 <body>
